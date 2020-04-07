c25461225d397bc3fff8869987e695e544fcf652
Amber W <amber.wolf@outlook.com>
2020-04-07 10:54:35 -0600

    Add practice file for page 56 of C# & .NET FRAMEWORK COURSE

---
+++ sample/Assignment1.cs
@@ -0,0 +1,10 @@
+using System;
+
+class SampleApplication
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems!");
+		Console.WriteLine("Remember, we're accounting on you!");
+    }
+}
