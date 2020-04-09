96676042c96c63f7c06c861146b1ac588e044447
Amber W <amber.wolf@outlook.com>
2020-04-09 09:13:49 -0600

    add assignments project

---
+++ Assignments/Assignments/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //************ assignment page 101 ************//
+
+            Console.WriteLine("What is your age?"); // ask the user age
+            int personAge = Convert.ToInt32(Console.ReadLine()); // assign the user input to personAge
+
+            Console.WriteLine("Have you ever had a DUI? Please answer with true or false"); // ask the user if they have had a dui
+            bool personDUI = Convert.ToBoolean(Console.ReadLine()); // assign the user input to personDUI- since it is boolean it must be true or false
+
+            Console.WriteLine("How many speeding tickets do you have?"); // ask the user number of tickets
+            int personTickets = Convert.ToInt32(Console.ReadLine()); // assign the user input to personTickets
+
+            if (personAge >= 15 && personTickets <= 3 && personDUI == false) //If statement to determine the output- the person age must be equal to or greater than 15 AND number of tickets must be equal to
+            {                                                                  // or less than 3 AND DUI must be false.
+                Console.WriteLine("Congratulations, you are eligible for car insurance"); //if all of these conditions are met the console will print this.
+            }
+            else // if the conditions are not met it will print this.
+            {
+                Console.WriteLine("Sorry, you are not eligible for car insurance");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+   
---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AND:");
+            Console.WriteLine("TRUE && FALSE:");
+            Console.WriteLine(true && false); // AND
+            Console.WriteLine("TRUE && TRUE:");
+            Console.WriteLine(true && true);
+            Console.WriteLine("FALSE && FALSE:");
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("OR:");
+            Console.WriteLine("TRUE || FALSE:");
+            Console.WriteLine(true || false); // OR
+            Console.WriteLine("TRUE || TRUE:");
+            Console.WriteLine(true || true);
+            Console.WriteLine("FALSE || FALSE:");
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("EQUAL:");
+            Console.WriteLine("TRUE == FALSE:");
+            Console.WriteLine(true == false); // EQUAL
+            Console.WriteLine("TRUE == TRUE:");
+            Console.WriteLine(true == true);
+            Console.WriteLine("FALSE == FALSE:");
+            Console.WriteLine(false == false);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("DOES NOT EQUAL:");
+            Console.WriteLine("TRUE != FALSE:");
+            Console.WriteLine(true != false); // DOES NOT EQUAL
+            Console.WriteLine("TRUE != TRUE:");
+            Console.WriteLine(true != true);
+            Console.WriteLine("FALSE != FALSE:");
+            Console.WriteLine(false != false);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("EXCLUSIVE OR:");
+            Console.WriteLine("TRUE ^ FALSE:");
+            Console.WriteLine(true ^ false); // EXCLUSIVE OR aka XOR
+            Console.WriteLine("TRUE ^ TRUE:"); // Evaluates the true- it is exclusively one or the other. Not both. so true and true would be false. 
+            Console.WriteLine(true ^ true);
+            Console.WriteLine("FALSE ^ FALSE:");
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
