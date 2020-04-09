ab7e77892390da03de2338bd0e3571b2f9dd5b2e
Amber W <amber.wolf@outlook.com>
2020-04-09 15:33:59 -0600

    add project for boolean comparison

--- Assignments/Assignments/Program.cs
+++ Assignments/Assignments/Program.cs
@@ -33,43 +33,107 @@ namespace Assignments
 
             //Console.ReadLine();
 
-            //************ assignment page 123 ************//
+            ////************ assignment page 123 commented out ************//
 
-            Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); // intro line
+            //Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); // intro line
 
-            Console.WriteLine(Environment.NewLine); //leaves a blank line between the next line
+            //Console.WriteLine(Environment.NewLine); //leaves a blank line between the next line
 
-            Console.WriteLine("What is the Weight of the package?"); // ask the user for the weight
-            double packageWeight = Convert.ToDouble(Console.ReadLine()); // allows the weight to be a decimal i.e 4.5lbs
+            //Console.WriteLine("What is the Weight of the package?"); // ask the user for the weight
+            //double packageWeight = Convert.ToDouble(Console.ReadLine()); // allows the weight to be a decimal i.e 4.5lbs
 
-            if (packageWeight > 50) // if the package weight is greater than this
+            //if (packageWeight > 50) // if the package weight is greater than this
+            //{
+            //    Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day."); // end the program with this statement
+            //}
+            //else // if it is not greater than 50 do this
+            //{
+            //    Console.WriteLine("What is the Width of the package?"); // ask the width of the package
+            //    double packageWidth = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
+
+            //    Console.WriteLine("What is the Length of the package?"); // ask the length of the package
+            //    double packageLength = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
+
+            //    Console.WriteLine("What is the Height of the package?"); // ask the height of the package
+            //    double packageHeight = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
+
+            //    double boxDimensions = packageWidth * packageLength * packageHeight; // dimension of a box is L X W X H
+            //    double estimatedPrice = boxDimensions * packageWeight / 100; // To get the price take the box dimension * by weight and then divide
+
+            //    if (boxDimensions > 50) // If the result of boxDimensions is greater than this it will
+            //    {
+            //        Console.WriteLine("Your package is too big to be shipped via Package Express."); // stop the program and end with this message
+            //    }
+            //    else
+            //    { // If it is not greater than 50
+            //        Console.WriteLine("Your estimated total for shipping this package is: $" + estimatedPrice); // it will return the estimated total
+            //    }
+
+            //}
+            //Console.ReadLine(); // Allows the console to keep the message displayed
+
+            ////************ assignment page 126 ************/////
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            string validatedAnimal= animal.ToUpper(); // forces the user input to be all caps. Allows the user to enter cat CAT or Cat and still get bool of true
+            bool isGuessed = validatedAnimal == "DOG" || validatedAnimal == "DOGS"; // when the variable number is equal to DOG or DOGS bool will be true
+
+            // The code as a while statement //
+            while (!isGuessed) // while isGuessed is false do the following
             {
-                Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day."); // end the program with this statement
-            }                                                                                                       
-            else // if it is not greater than 50 do this
-            {
-                Console.WriteLine("What is the Width of the package?"); // ask the width of the package
-                double packageWidth = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
-
-                Console.WriteLine("What is the Length of the package?"); // ask the length of the package
-                double packageLength = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
-
-                Console.WriteLine("What is the Height of the package?"); // ask the height of the package
-                double packageHeight = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
-
-                double boxDimensions = packageWidth * packageLength * packageHeight; // dimension of a box is L X W X H
-                double estimatedPrice = boxDimensions * packageWeight / 100; // To get the price take the box dimension * by weight and then divide
-                                                                              
-                if (boxDimensions > 50) // If the result of boxDimensions is greater than this it will
+                switch (validatedAnimal)    
                 {
-                    Console.WriteLine("Your package is too big to be shipped via Package Express."); // stop the program and end with this message
+                    case "DOG": //if the user entry is dog
+                        Console.WriteLine("Dogs are my favorite too! Aren't they great?");
+                        isGuessed = true;
+                        break;
+                        
+                    case "DOGS": // if the user entry is dogs
+                        Console.WriteLine("Dogs are my favorite too! Aren't they great?");
+                        isGuessed = true;
+                        break;
+
+                    default: // if none of the cases are correct this is the default
+                        Console.WriteLine(animal + "s are pretty great! But I think another animal is better.");
+                        Console.WriteLine("Do you have another favorite animal?");
+                        animal = Console.ReadLine(); // allows user to update animal to a different input
+                        validatedAnimal = animal.ToUpper(); // validates the new input to be uppercase
+                        break;
                 }
-                else { // If it is not greater than 50
-                    Console.WriteLine("Your estimated total for shipping this package is: $" + estimatedPrice); // it will return the estimated total
-                }                                                                                                
+            }
+
+
+            // The same code redone a do while statement //
+
+            do // ensures the loop happens at least once. 
+            {
+
+                switch (validatedAnimal)
+                {
+                    case "DOG":
+                        Console.WriteLine("Dogs are my favorite too! Aren't they great?");
+                        isGuessed = true;
+                        break;
+
+                    case "DOGS":
+                        Console.WriteLine("Dogs are my favorite too! Aren't they great?");
+                        isGuessed = true;
+                        break;
+
+                    default: // if none of the cases are correct this is the default
+                        Console.WriteLine(animal + "s are pretty great! But I think another animal is better.");
+                        Console.WriteLine("Do you have another favorite animal?");
+                        animal = Console.ReadLine();
+                        validatedAnimal = animal.ToUpper();
+                        break;
+                }
 
             }
-            Console.ReadLine(); // Allows the console to keep the message displayed
+            while (!isGuessed);
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch statement
+
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12; // when the variable number is equal to 12 bool will be true
+
+            do // this makes sure the loop is done at least once. This means it will hit case 12- otherwise when you don't have the do statement and 12 is guessed first it will never initiate the while loop
+            { // to return case 12. This ensures it happens at least once.
+                switch (number)
+                {
+                    case 62: //if number is 62
+                        Console.WriteLine("You guessed 62. Guess again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine()); // update the variable number to the new number
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. That is correct!");
+                        isGuessed = true; // updates the isGuessed variable to true so the while loop stops
+                        break;
+                    default: // if none of the cases are correct this is the default
+                        Console.WriteLine("That is incorrect. Guess again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }// end switch 
+            }
+            while (!isGuessed); // the !isguessed means the same as isGuessed=false. The while loop will run while isGuessed = not true. 
+            Console.ReadLine();
+        }
+    }
+}
