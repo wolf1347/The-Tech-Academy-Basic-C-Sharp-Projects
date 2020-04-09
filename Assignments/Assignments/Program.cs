7da0dc992e1587dab57ff6be3f1a047d9e2315de
Amber W <amber.wolf@outlook.com>
2020-04-09 12:15:27 -0600

    Add shipping assignment

--- Assignments/Assignments/Program.cs
+++ Assignments/Assignments/Program.cs
@@ -11,27 +11,65 @@ namespace Assignments
         static void Main(string[] args)
         {
 
-            //************ assignment page 101 ************//
+            ////************ assignment page 101 commented out ************//
 
-            Console.WriteLine("What is your age?"); // ask the user age
-            int personAge = Convert.ToInt32(Console.ReadLine()); // assign the user input to personAge
+            //Console.WriteLine("What is your age?"); // ask the user age
+            //int personAge = Convert.ToInt32(Console.ReadLine()); // assign the user input to personAge
 
-            Console.WriteLine("Have you ever had a DUI? Please answer with true or false"); // ask the user if they have had a dui
-            bool personDUI = Convert.ToBoolean(Console.ReadLine()); // assign the user input to personDUI- since it is boolean it must be true or false
+            //Console.WriteLine("Have you ever had a DUI? Please answer with true or false"); // ask the user if they have had a dui
+            //bool personDUI = Convert.ToBoolean(Console.ReadLine()); // assign the user input to personDUI- since it is boolean it must be true or false
 
-            Console.WriteLine("How many speeding tickets do you have?"); // ask the user number of tickets
-            int personTickets = Convert.ToInt32(Console.ReadLine()); // assign the user input to personTickets
+            //Console.WriteLine("How many speeding tickets do you have?"); // ask the user number of tickets
+            //int personTickets = Convert.ToInt32(Console.ReadLine()); // assign the user input to personTickets
 
-            if (personAge >= 15 && personTickets <= 3 && personDUI == false) //If statement to determine the output- the person age must be equal to or greater than 15 AND number of tickets must be equal to
-            {                                                                  // or less than 3 AND DUI must be false.
-                Console.WriteLine("Congratulations, you are eligible for car insurance"); //if all of these conditions are met the console will print this.
-            }
-            else // if the conditions are not met it will print this.
+            //if (personAge >= 15 && personTickets <= 3 && personDUI == false) //If statement to determine the output- the person age must be equal to or greater than 15 AND number of tickets must be equal to
+            //{                                                                  // or less than 3 AND DUI must be false.
+            //    Console.WriteLine("Congratulations, you are eligible for car insurance"); //if all of these conditions are met the console will print this.
+            //}
+            //else // if the conditions are not met it will print this.
+            //{
+            //    Console.WriteLine("Sorry, you are not eligible for car insurance");
+            //}
+
+            //Console.ReadLine();
+
+            //************ assignment page 123 ************//
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below."); // intro line
+
+            Console.WriteLine(Environment.NewLine); //leaves a blank line between the next line
+
+            Console.WriteLine("What is the Weight of the package?"); // ask the user for the weight
+            double packageWeight = Convert.ToDouble(Console.ReadLine()); // allows the weight to be a decimal i.e 4.5lbs
+
+            if (packageWeight > 50) // if the package weight is greater than this
             {
-                Console.WriteLine("Sorry, you are not eligible for car insurance");
-            }
+                Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day."); // end the program with this statement
+            }                                                                                                       
+            else // if it is not greater than 50 do this
+            {
+                Console.WriteLine("What is the Width of the package?"); // ask the width of the package
+                double packageWidth = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
 
-            Console.ReadLine();
+                Console.WriteLine("What is the Length of the package?"); // ask the length of the package
+                double packageLength = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
+
+                Console.WriteLine("What is the Height of the package?"); // ask the height of the package
+                double packageHeight = Convert.ToDouble(Console.ReadLine()); // allows the input to be a decimal
+
+                double boxDimensions = packageWidth * packageLength * packageHeight; // dimension of a box is L X W X H
+                double estimatedPrice = boxDimensions * packageWeight / 100; // To get the price take the box dimension * by weight and then divide
+                                                                              
+                if (boxDimensions > 50) // If the result of boxDimensions is greater than this it will
+                {
+                    Console.WriteLine("Your package is too big to be shipped via Package Express."); // stop the program and end with this message
+                }
+                else { // If it is not greater than 50
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + estimatedPrice); // it will return the estimated total
+                }                                                                                                
+
+            }
+            Console.ReadLine(); // Allows the console to keep the message displayed
         }
     }
 }
---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using if else and else if statements
+
+            //int currentTemperature = 70;
+            //int roomTemperature = 70;
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");         
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //    {
+            //    Console.WriteLine("It is warmer than room temperature");
+            //}
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature");
+            //}
+            //Console.ReadLine();
+
+            ////does the same thing but in a single line of code using ? "what is the response when true" : "what is the response when false"
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temperature" : "It is not room temperature";
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is room temperature where you are");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature where you are");
+            //}
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("It is cooler than room temperature where you are");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("error 404 temperature not found");
+            //}
+            //Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            int favoriteNumber = Convert.ToInt32(Console.ReadLine());
+
+            string results = favoriteNumber == 5 ? "Five is my favorite number too" : "I guess that is a good number but five is better";
+            Console.WriteLine(results);
+            Console.ReadLine();
+        }
+    }
+}
