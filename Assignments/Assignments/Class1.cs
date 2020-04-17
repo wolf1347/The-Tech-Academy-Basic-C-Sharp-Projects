bb48ab27948a5a0d274fc24b510ccedfa0f79f97
Amber W <amber.wolf@outlook.com>
2020-04-17 12:02:34 -0600

    add class assignment

---
+++ Assignments/Assignments/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    class Math // new math class that can be called on by the main program
+    {
+        public int addition(int number1, int number2) { // create function to add 2 numbers
+            return number1 + number2;
+        } 
+
+        public int subtraction(int number3, int number4) // create function to subtract 2 numbers
+        {  // create function to add 2 numbers
+            return number3 - number4;
+        }
+
+        public int multiplication(int number5, int number6) // create function to multiply 2 numbers
+        {
+            return number5 * number6;
+        }
+    }
+}
+
--- Assignments/Assignments/Program.cs
+++ Assignments/Assignments/Program.cs
@@ -76,13 +76,13 @@ namespace Assignments
 
             //Console.WriteLine("What is your favorite animal?");
             //string animal = Console.ReadLine();
-            //string validatedAnimal= animal.ToUpper(); // forces the user input to be all caps. Allows the user to enter cat CAT or Cat and still get bool of true
+            //string validatedAnimal = animal.ToUpper(); // forces the user input to be all caps. Allows the user to enter cat CAT or Cat and still get bool of true
             //bool isGuessed = validatedAnimal == "DOG" || validatedAnimal == "DOGS"; // when the variable number is equal to DOG or DOGS bool will be true
 
             //// The code as a while statement //
             //while (!isGuessed) // while isGuessed is false do the following
             //{
-            //    switch (validatedAnimal)    
+            //    switch (validatedAnimal)
             //    {
             //        case "DOG": //if the user entry is dog
             //            Console.WriteLine("Dogs are my favorite too! Aren't they great?");
@@ -167,7 +167,7 @@ namespace Assignments
 
             ////************ assignment page 136 COMMENTED OUT************//
 
-            // creating the string array
+            ////creating the string array
 
             //string[] stringArray = { "cat", "dog", "bird", "hamster", "lizard", "snake", "fish" }; // create a string array 
 
@@ -185,7 +185,7 @@ namespace Assignments
 
             ////creating the int array
 
-            //int[] intArray = { 5, 10, 15, 20, 25 , 30, 35 }; // create an int array 
+            //int[] intArray = { 5, 10, 15, 20, 25, 30, 35 }; // create an int array 
 
             //Console.WriteLine("Please select an index number from the integer array starting with 0 and ending with 6"); // ask the user to make a selection
             //int intSelection = Convert.ToInt32(Console.ReadLine()); // converts the selection from a string to an integer which allows it to be used in the print from the array
@@ -224,150 +224,185 @@ namespace Assignments
 
             ////************ assignment page 142 COMMENTED OUT************//
 
-            //  string[] bestVideoGames = { "Mass Effect 2", "Animal Crossing New Leaf", "Overwatch", "Monster Rancher 2", "Earthbound", "Dark Cloud", "Pokemon Crystal Version", "Borderlands 2", 
+            //string[] bestVideoGames = { "Mass Effect 2", "Animal Crossing New Leaf", "Overwatch", "Monster Rancher 2", "Earthbound", "Dark Cloud", "Pokemon Crystal Version", "Borderlands 2",
             //      "Deadly Premonition", "Ratchet and Clank a Crack in Time", "The Elder Scrolls: Oblivion", "Phoenix Wright Ace Attorney: Justice for All"}; // create a string array
 
 
-            //  Console.WriteLine("Array title is \"Best Video Games\". Please enter some text to add to an item in the array.");
-            //  string userArrayText = Console.ReadLine();
+            //Console.WriteLine("Array title is \"Best Video Games\". Please enter some text to add to an item in the array.");
+            //string userArrayText = Console.ReadLine();
 
-            //  for (int i = 0; i < bestVideoGames.Length; i++)
-            //  {
-            //      Console.Write(bestVideoGames[i] + " " + userArrayText + "\n");
-            //  }
+            //for (int i = 0; i < bestVideoGames.Length; i++)
+            //{
+            //    Console.Write(bestVideoGames[i] + " " + userArrayText + "\n");
+            //}
 
-            //  Console.ReadLine();
+            //Console.ReadLine();
 
-            //  //while (true)  // 
-            //  //{
-            //  //    Console.WriteLine("Hello"); //// commented out the infinite loop
-            //  // }
-            //  //Console.ReadLine();
+            ////while (true)  // 
+            ////{
+            ////    Console.WriteLine("Hello"); //// commented out the infinite loop
+            //// }
+            ////Console.ReadLine();
 
-            //  bool infiniteLoop = true;
+            //bool infiniteLoop = true;
 
-            //  while (infiniteLoop)  // 
-            //  {
-            //      Console.WriteLine("Hello"); // here is the infinite loop corrected to run once
-            //      infiniteLoop = false;
-            //  }
+            //while (infiniteLoop)  // 
+            //{
+            //    Console.WriteLine("Hello"); // here is the infinite loop corrected to run once
+            //    infiniteLoop = false;
+            //}
 
-            //  Console.WriteLine("\n"); // adds a blank line for readablility
+            //Console.WriteLine("\n"); // adds a blank line for readablility
 
-            //  for (int i = 0; i < 10; i++) // while i is less than 10
-            //  {
-            //      Console.WriteLine(i); // do this
-            //  }
+            //for (int i = 0; i < 10; i++) // while i is less than 10
+            //{
+            //    Console.WriteLine(i); // do this
+            //}
 
-            //  Console.WriteLine("\n"); // adds a blank line for readablility
+            //Console.WriteLine("\n"); // adds a blank line for readablility
 
-            //              for (int i = 0; i <= 10; i++) // while i is less than or equal to 10
-            //  {
-            //      Console.WriteLine(i); // do this
-            //  }
+            //for (int i = 0; i <= 10; i++) // while i is less than or equal to 10
+            //{
+            //    Console.WriteLine(i); // do this
+            //}
 
             //// list created with no duplicates
 
-            //  List<string> favoriteGames = new List<string>() {"Mass Effect 2", "Animal Crossing New Leaf", "Overwatch", "Monster Rancher 2", "Earthbound", "Dark Cloud", "Pokemon Crystal Version", "Borderlands 2",
+            //List<string> favoriteGames = new List<string>() {"Mass Effect 2", "Animal Crossing New Leaf", "Overwatch", "Monster Rancher 2", "Earthbound", "Dark Cloud", "Pokemon Crystal Version", "Borderlands 2",
             //      "Deadly Premonition", "Ratchet and Clank a Crack in Time", "The Elder Scrolls: Oblivion", "Phoenix Wright Ace Attorney: Justice for All"};
 
-            //  Console.WriteLine("Please search my list of favorite video games to see if you find a match."); // ask the user to search for a game
-            //  string userGameSearch = Console.ReadLine(); // assign their search to userGameSearch
-            //  int index = favoriteGames.FindIndex(a => a.Contains(userGameSearch)); // If the search is found it will assign that games index # in the list to index
+            //Console.WriteLine("Please search my list of favorite video games to see if you find a match."); // ask the user to search for a game
+            //string userGameSearch = Console.ReadLine(); // assign their search to userGameSearch
+            //int index = favoriteGames.FindIndex(a => a.Contains(userGameSearch)); // If the search is found it will assign that games index # in the list to index
 
 
-            //  foreach (string game in favoriteGames) // for each index in favoriteGames is referred to as game
-            //  {
-            //      if (favoriteGames.Contains(userGameSearch)) // if user search is equal to a game in the list
-            //      {
-            //          Console.WriteLine(userGameSearch + " is one of my favorites too! The index number is " + index); // write this
-            //          Console.ReadLine();
-            //          if(favoriteGames.Contains(userGameSearch)) // Not sure if I used this one correctly- the intention is it will check if the favoriteGames list contains userGameSearch. And if it does, stop the code with a break.
-            //          {
-            //              break;
-            //              }                    
-            //      }
-            //      else // otherwise
-            //      {
-            //          Console.WriteLine("Sorry, " + userGameSearch + " is not on my list."); // write this
-            //          break; // ends this code
-            //      }
+            //foreach (string game in favoriteGames) // for each index in favoriteGames is referred to as game
+            //{
+            //    if (favoriteGames.Contains(userGameSearch)) // if user search is equal to a game in the list
+            //    {
+            //        Console.WriteLine(userGameSearch + " is one of my favorites too! The index number is " + index); // write this
+            //        Console.ReadLine();
+            //        if (favoriteGames.Contains(userGameSearch)) // Not sure if I used this one correctly- the intention is it will check if the favoriteGames list contains userGameSearch. And if it does, stop the code with a break.
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    else // otherwise
+            //    {
+            //        Console.WriteLine("Sorry, " + userGameSearch + " is not on my list."); // write this
+            //        break; // ends this code
+            //    }
 
-            //  } //end foreach
+            //} //end foreach
 
-            //  Console.WriteLine("\n"); // adds a blank line for readablility
+            //Console.WriteLine("\n"); // adds a blank line for readablility
 
-            //  //list with duplicates 
+            ////list with duplicates 
 
-            //  List<string> housePets = new List<string>() {"Deku","Jazz","Sweet Pea","Leo","Trixie","Deku"};  //Deku is in here twice
+            //List<string> housePets = new List<string>() { "Deku", "Jazz", "Sweet Pea", "Leo", "Trixie", "Deku" };  //Deku is in here twice
 
-            //      Console.WriteLine("I have 5 pets. Deku, Jazz, Trixie, Sweet Pea, and Leo. Please select one to search the list for."); // ask the user to search for a game
-            //      string petSearch = Console.ReadLine(); // assign their search to petSearch
-            //      int pets = housePets.FindIndex(a => a.Contains(petSearch)); // If the search is found it will assign that games index # in the list to index
+            //Console.WriteLine("I have 5 pets. Deku, Jazz, Trixie, Sweet Pea, and Leo. Please select one to search the list for."); // ask the user to search for a game
+            //string petSearch = Console.ReadLine(); // assign their search to petSearch
+            //int pets = housePets.FindIndex(a => a.Contains(petSearch)); // If the search is found it will assign that games index # in the list to index
 
 
-            //      foreach (string pet in housePets) // for each index in favoriteGames is referred to as game
-            //      {
-            //          if (housePets.Contains(petSearch)) // if user search is equal to a pet in the list
-            //          {
-            //              Console.WriteLine(petSearch + " " + pets); // write this //- when the list has duplicates it seems to stop at the first instance of finding "Deku"
-            //              Console.ReadLine();
-            //              break;
-            //          }
-            //          else // otherwise
-            //          {
-            //              Console.WriteLine("Sorry, " + petSearch + " is not one of my pets"); // write this
-            //              break;
-            //          }
+            //foreach (string pet in housePets) // for each index in favoriteGames is referred to as game
+            //{
+            //    if (housePets.Contains(petSearch)) // if user search is equal to a pet in the list
+            //    {
+            //        Console.WriteLine(petSearch + " " + pets); // write this //- when the list has duplicates it seems to stop at the first instance of finding "Deku"
+            //        Console.ReadLine();
+            //        break;
+            //    }
+            //    else // otherwise
+            //    {
+            //        Console.WriteLine("Sorry, " + petSearch + " is not one of my pets"); // write this
+            //        break;
+            //    }
 
-            //      }// end foreach
+            //}// end foreach
 
-            //  Console.WriteLine("\n"); // adds a blank line for readablility
+            //Console.WriteLine("\n"); // adds a blank line for readablility
 
-            //  // string list with duplicate search for dups
+            //// string list with duplicate search for dups
 
-            //  List<string> dinnerChoices = new List<string>() { "Tacos", "Ramen", "Breakfast for Dinner", "Soup and Salad", "Spaghetti", "Spaghetti" };  //Deku is in here twice
-            //  var dinner = dinnerChoices.GroupBy(i => i);
-            //  foreach (var food in dinner)
-            //  {
-            //      Console.WriteLine("{0} {1}", food.Key, food.Count()); // counts the number of times this item is on the list. Since Spaghetti is on there twice the count will return 2 to indicate a duplicate
-            //  }
+            //List<string> dinnerChoices = new List<string>() { "Tacos", "Ramen", "Breakfast for Dinner", "Soup and Salad", "Spaghetti", "Spaghetti" };  //Deku is in here twice
+            //var dinner = dinnerChoices.GroupBy(i => i);
+            //foreach (var food in dinner)
+            //{
+            //    Console.WriteLine("{0} {1}", food.Key, food.Count()); // counts the number of times this item is on the list. Since Spaghetti is on there twice the count will return 2 to indicate a duplicate
+            //}
 
-            //  Console.ReadLine();
+            //Console.ReadLine();
 
-            //************ assignment page 147************//
+            ////************ assignment page 147 COMMENTED OUT************//
 
-            List<int> divideNumbers = new List<int>() {75, 100, 250, 425}; // intinger list
+            //List<int> divideNumbers = new List<int>() { 75, 100, 250, 425 }; // intinger list
 
-           
-            try // try to do the foreEach
-            {
-                Console.WriteLine("Please enter a number to divide by a number on our list. The resulting number will be rounded"); // asks the user for a number
-                int userNumber = Convert.ToInt32(Console.ReadLine()); // converts the user input to a int so it can be used in a math equation.
 
-                foreach (int number in divideNumbers) // will loop through the list
-                {
-                    int dividedNumber = number / userNumber; // divide the number on the list to the user's entered number and assigns it in divideNumber
-                    Console.WriteLine(number + " divided by " + userNumber + " is equal to " + dividedNumber); // prints this message to the screen to show the result
-                }
-            }
-            catch (FormatException) // if there is a format exception
-            {
-                Console.WriteLine("Please enter a number. Cannot divide a number by a string.\n"); // print this message
-            }
-            catch (DivideByZeroException) // if there is a divide by zero exception
-            {
-                Console.WriteLine("Unable to divide by zero"); // print this message to the screen
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message); // otherwise print the standard generic message
-            }
-            finally // ensures this will always run
-            {
-                Console.WriteLine("\nIf you can see this that means the finally has been hit after they try catch"); //write a message to show this still hits even after an exception in the try catch
-                Console.ReadLine(); // ensures the Console.ReadLine is always hit
-            }
+            //try // try to do the foreEach
+            //{
+            //    Console.WriteLine("Please enter a number to divide by a number on our list. The resulting number will be rounded"); // asks the user for a number
+            //    int userNumber = Convert.ToInt32(Console.ReadLine()); // converts the user input to a int so it can be used in a math equation.
+
+            //    foreach (int number in divideNumbers) // will loop through the list
+            //    {
+            //        int dividedNumber = number / userNumber; // divide the number on the list to the user's entered number and assigns it in divideNumber
+            //        Console.WriteLine(number + " divided by " + userNumber + " is equal to " + dividedNumber); // prints this message to the screen to show the result
+            //    }
+            //}
+            //catch (FormatException) // if there is a format exception
+            //{
+            //    Console.WriteLine("Please enter a number. Cannot divide a number by a string.\n"); // print this message
+            //}
+            //catch (DivideByZeroException) // if there is a divide by zero exception
+            //{
+            //    Console.WriteLine("Unable to divide by zero"); // print this message to the screen
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message); // otherwise print the standard generic message
+            //}
+            //finally // ensures this will always run
+            //{
+            //    Console.WriteLine("\nIf you can see this that means the finally has been hit after they try catch"); //write a message to show this still hits even after an exception in the try catch
+            //    Console.ReadLine(); // ensures the Console.ReadLine is always hit
+            //}
+
+            //************ assignment page 156************//
+
+            Math mathFunctions = new Math(); // calls on Math which can be called on by mathFunctions
+
+            Console.WriteLine("Please enter a number"); // ask the user for a number
+            int userNumber1 = Convert.ToInt32(Console.ReadLine()); // store that number
+            Console.WriteLine("Please enter a second number"); // ask for a second number
+            int userNumber2 = Convert.ToInt32(Console.ReadLine()); // store that number
+
+            
+            int additionResult = mathFunctions.addition(userNumber1, userNumber2); // calls on the addition function in math
+            Console.WriteLine(userNumber1 + " + " + userNumber2 + " is equal to " + additionResult); // prints the results
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a number");
+            int userNumber3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int userNumber4 = Convert.ToInt32(Console.ReadLine());
+
+
+            int subtractionResult = mathFunctions.subtraction(userNumber3, userNumber4); // calls on the subtraction function in math
+            Console.WriteLine(userNumber3 + " - " + userNumber4 + " is equal to " + subtractionResult); // prints the results
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number");
+            int userNumber5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int userNumber6 = Convert.ToInt32(Console.ReadLine());
+
+
+            int multiplyResults = mathFunctions.multiplication(userNumber5, userNumber6); // calls on the multiplication function in math
+            Console.WriteLine(userNumber5 + " * " + userNumber6 + " is equal to " + multiplyResults); // prints the results
+            Console.ReadLine();
         }
     }
 }
