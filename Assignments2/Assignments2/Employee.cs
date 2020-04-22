4d17b34d3992f3a025e557f4c6eb209e043b6556
Amber W <amber.wolf@outlook.com>
2020-04-22 12:19:23 -0600

    add overload operator

--- Assignments2/Assignments2/Employee.cs
+++ Assignments2/Assignments2/Employee.cs
@@ -8,6 +8,9 @@ namespace Assignments2
 {
     public class Employee: Person, IQuittable // inherits from both Person (the parent class) and IQuittable (the interface)
     {
+        public int Id { get; set; }
+        public List<string> Employees{ get; set; }
+
         public override void SayName() //creates the SayName function which is required of the Person parent class
         {
             Console.WriteLine("Employee information \n"); // adds to the SayName function it inherits from the parent class with an Employee specific addition
@@ -23,5 +26,36 @@ namespace Assignments2
         {
             Console.WriteLine(employee.FirstName + " " + employee.LastName + " has quit.");
         }
+
+        public static Boolean operator ==(Employee employee1, Employee employee2) // overload of the == operator to return a boolean value
+        {
+            if (employee1.Id == employee2.Id) // if the IDs are equal
+            {
+                Console.WriteLine("The employee IDs match so this is the same employee"); // write this to the console and return true
+                return true;
+            }
+            else {
+                
+                Console.WriteLine("The employee IDs do not match so this is not the same employee"); // otherwise if they are not equal write this to the console and return false
+                return false;
+            }
+
+        }
+
+        public static Boolean operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id) // overload of the != operator to return a boolean value
+            {
+                Console.WriteLine("he employee IDs do not match so this is not the same employee"); // if the IDs are not equal
+                return true; // write this to the console and return true
+            }
+            else 
+            {
+                Console.WriteLine("The employee IDs match so this is the same employee"); // otherwise if they are equal write this to the console and return false
+                return false;
+            }
+
+        }
+
     }
 }
--- Assignments2/Assignments2/Program.cs
+++ Assignments2/Assignments2/Program.cs
@@ -18,16 +18,36 @@ namespace Assignments2
             //employee.SayName();
             //Console.ReadLine();
 
-            //********** Assignment Page 179**********//
+            //********** Assignment Page 179 COMMENTED OUT**********//
 
 
-            Employee employee = new Employee(); // calls on the Employee class which will be called employee
-            employee.FirstName = "Sample"; // sets First Name to Sample
-            employee.LastName = "Student"; // sets Last Name to Student
-            employee.Quit(employee); // calls on the Quit method in Employee which was inherited via the IQuittable interface. Typically Employee would only be able to inherit one class, currently set to Person, but
-            //using the interface allows it to inherit from IQuittable as well and take the Quit function.
+            //Employee employee = new Employee(); // calls on the Employee class which will be called employee
+            //employee.FirstName = "Sample"; // sets First Name to Sample
+            //employee.LastName = "Student"; // sets Last Name to Student
+            //employee.Quit(employee); // calls on the Quit method in Employee which was inherited via the IQuittable interface. Typically Employee would only be able to inherit one class, currently set to Person, but
+            ////using the interface allows it to inherit from IQuittable as well and take the Quit function.
+            //Console.ReadLine();
+
+
+            //********** Assignment Page 184**********//
+
+            var employee1 = new Employee {FirstName="Amber", LastName="Wolf", Id=01 }; // instantiate the Employee class in to employee1
+            var employee2 = new Employee {FirstName = "Deku", LastName = "Wolf", Id = 02 }; // instantiate the Employee class in to employee1
+
+            List<Employee> employeeList = new List<Employee>() // initialize a list and add the new instances of Employee in to it
+            {
+               employee1,
+               employee2
+            };
+
+
+            if (employee1 == employee2) // compare statement using the overload ==
+            {
+
+            }
+            
+
             Console.ReadLine();
-
         }
     }
 }
