6e43a8b48d47961ecee911114ed08b884a1cb434
Amber W <amber.wolf@outlook.com>
2020-05-05 18:41:39 -0600

    add knockout practice

---
+++ Knockout/Knockout/Scripts/Clicks.js
@@ -0,0 +1,25 @@
+﻿var ClickCounterViewModel = function () {
+    this.numberOfClicks = ko.observable(0);
+
+    this.registerClick = function () {
+        this.numberOfClicks(this.numberOfClicks() + 1); // adds one to the counter each time you click
+    };
+
+    this.resetClicks = function () {
+        this.numberOfClicks(0); // resets the clicks to 0
+    };
+
+    this.viewCat = ko.computed(function () {
+        return this.numberOfClicks() < 8; // if the clicks are less than 8 you can call on the viewCat function.
+    }, this);
+
+    this.hasClickedTooManyTimes = ko.computed(function () {
+        return this.numberOfClicks() >= 8; // if clicks are greater than or equal to 8 you can call too many clicks
+    }, this);
+
+
+
+    
+};
+
+ko.applyBindings(new ClickCounterViewModel());
